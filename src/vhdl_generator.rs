9cfb7551584a54cb5af3ec171eb9eb7b9a5d2b27
Johannes Kutning <120175758+JohannesKutning@users.noreply.github.com>
2023-01-13 14:18:48 +0100

    Adds stubs for vhdl code generation

---
+++ src/vhdl_generator.rs
@@ -0,0 +1,21 @@
+pub struct VhdlGenerator {
+    sources : Vec< VhdlFile >
+}
+
+impl VhdlGenerator {
+    pub fn new() -> VhdlGenerator {
+        VhdlGenerator { sources : Vec::new)( }
+    }
+
+    pub fn create_source_code( & mut self, description : & ModuleDescription ) -> Result< (), Box< dyn Error > > {
+
+        Ok(())
+    }
+
+    pub fn write_source_files( & self, description : & ModuleDescription ) -> Result< (), Box< dyn Error > > {
+
+        Ok(())
+    }
+}
+
+
